600128eacc38f24bd435aa6c8c87518d8c92d2a3
adib <asz.25@hotmail.com>
2021-02-26 00:39:10 +0800

    Implement the main method

---
+++ PongGame.java
@@ -0,0 +1,14 @@
+//This is the main method
+
+
+
+public class PongGame {
+
+	public static void main(String[] args) {
+		
+		
+		//Create class object of the frame
+		GameFrame frame = new GameFrame();
+		
+	}
+}
